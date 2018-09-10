3f2695203e667d8d92b2f17866770ec6c55015a8
Joe Yennaco <joe@joeyennaco.com>
2018-09-10 19:23:04 -0400

    Moved ip_addr into bash.py

--- pycons3rt/bash.py
+++ pycons3rt/bash.py
@@ -21,7 +21,6 @@ import platform
 from threading import Timer
 
 from logify import Logify
-from aliasip import ip_addr
 
 __author__ = 'Joe Yennaco'
 
@@ -155,6 +154,80 @@ def run_command(command, timeout_sec=3600.0, output=True):
             'code': code
         }
     return output
+
+
+def validate_ip_address(ip_address):
+    """Validate the ip_address
+
+    :param ip_address: (str) IP address
+    :return: (bool) True if the ip_address is valid
+    """
+    # Validate the IP address
+    log = logging.getLogger(mod_logger + '.validate_ip_address')
+    if not isinstance(ip_address, basestring):
+        log.warn('ip_address argument is not a string')
+        return False
+
+    # Ensure there are 3 dots
+    num_dots = 0
+    for c in ip_address:
+        if c == '.':
+            num_dots += 1
+    if num_dots != 3:
+        log.info('Not a valid IP address: {i}'.format(i=ip_address))
+        return False
+
+    # Use the socket module to test
+    try:
+        socket.inet_aton(ip_address)
+    except socket.error as e:
+        log.info('Not a valid IP address: {i}\n{e}'.format(i=ip_address, e=e))
+        return False
+    else:
+        log.info('Validated IP address: %s', ip_address)
+        return True
+
+
+def ip_addr():
+    """Uses the ip addr command to enumerate IP addresses by device
+
+    :return: (dict) Containing device: ip_address
+    """
+    log = logging.getLogger(mod_logger + '.ip_addr')
+    log.debug('Running the ip addr command...')
+    ip_addr_output = {}
+
+    command = ['ip', 'addr']
+    try:
+        ip_addr_result = run_command(command, timeout_sec=20)
+    except CommandError:
+        _, ex, trace = sys.exc_info()
+        msg = 'There was a problem running command: {c}'.format(c=' '.join(command))
+        raise CommandError, msg, trace
+
+    ip_addr_lines = ip_addr_result['output'].split('\n')
+
+    for line in ip_addr_lines:
+        line = line.strip()
+        if line.startswith('inet6'):
+            continue
+        elif line.startswith('inet'):
+            parts = line.split()
+            try:
+                ip_address = parts[1].strip().split('/')[0]
+            except KeyError:
+                continue
+            else:
+                if not validate_ip_address(ip_address):
+                    continue
+                else:
+                    for part in parts:
+                        part = part.strip()
+                        if part.strip().startswith('eth') or part.strip().startswith('eno') or \
+                                part.strip().startswith('ens'):
+                            device = part
+                            ip_addr_output[device] = ip_address
+    return ip_addr_output
 
 
 def get_ip_addresses():
